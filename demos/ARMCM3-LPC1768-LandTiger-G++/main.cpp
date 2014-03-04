c1a39d0df25afbc3afd5ff83bf57bbe548bde057
Kong Fanbin <kfbuniversity@gmail.com>
2014-03-04 17:31:55 +0800

    Add CPP demo for LPC1768.

---
+++ demos/ARMCM3-LPC1768-LandTiger-G++/main.cpp
@@ -0,0 +1,101 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "ch.hpp"
+#include "hal.h"
+#include "test.h"
+
+#include "update.h"
+
+using namespace chibios_rt;
+/*===========================================================================*/
+/* Configurable settings.                                                    */
+/*===========================================================================*/
+
+/*
+ * Tester thread class. This thread executes the test suite.
+ */
+class TesterThread : public BaseStaticThread<256> {
+
+protected:
+  virtual msg_t main(void) {
+
+    setName("tester");
+
+    return TestThread(&SD1);
+  }
+
+public:
+  TesterThread(void) : BaseStaticThread<256>() {
+  }
+};
+
+static TesterThread tester;
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+  halInit();
+  chSysInit();
+
+  /*
+   * Test procedure.
+   */
+  LOG_PRINT("\n*** ChibiOS/RT IRQ-STORM long duration test\n");
+  LOG_PRINT("***\n");
+  LOG_PRINT("*** Kernel:       %s\n", CH_KERNEL_VERSION);
+#ifdef CH_COMPILER_NAME
+  LOG_PRINT("*** Compiler:     %s\n", CH_COMPILER_NAME);
+#endif
+  LOG_PRINT("*** Architecture: %s\n", CH_ARCHITECTURE_NAME);
+#ifdef CH_CORE_VARIANT_NAME
+  LOG_PRINT("*** Core Variant: %s\n", CH_CORE_VARIANT_NAME);
+#endif
+#ifdef CH_PORT_INFO
+  LOG_PRINT("*** Port Info:    %s\n", CH_PORT_INFO);
+#endif
+#ifdef PLATFORM_NAME
+  LOG_PRINT("*** Platform:     %s\n", PLATFORM_NAME);
+#endif
+#ifdef BOARD_NAME
+  LOG_PRINT("*** Test Board:   %s\n", BOARD_NAME);
+#endif
+  LOG_PRINT("***\n");
+  LOG_PRINT("*** System Clock: %u\n", LPC17xx_CCLK);
+  LOG_PRINT("*** Peripheral Clock: %u\n", LPC17xx_PCLK);
+  /*
+   * Normal main() thread activity, nothing in this test.
+   */
+
+  updateThreadStart();
+
+  tester.start(NORMALPRIO);
+  tester.wait();
+
+  while (TRUE) {
+    BaseThread::sleep(MS2ST(500));
+  }
+  return 0;
+}
---
+++ demos/ARMCM3-LPC1768-LandTiger-G++/ver.h
@@ -0,0 +1,12 @@
+#ifndef _VER_H_
+#define _VER_H_
+
+/*
+ * bits 8~15: System number
+ * bits 0~7:  Device number. 0xFE stands for common device
+ */
+#define DEVICE_ID 0xF101
+
+#define SW_VERSION 0x01
+
+#endif 	//_VER_H_
